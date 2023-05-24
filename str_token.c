b608f169ac98985d1727256d65b16914af537551
Shannon-Kioko <84199763+Shannon-Kioko@users.noreply.github.com>
2023-05-24 21:53:13 +0300

    Add str_token.c file

---
+++ str_token.c
@@ -0,0 +1,50 @@
+#include "shell.h"
+
+/**
+ * _strtok - Function that separates strings using delimiters
+ *
+ * @line: Pointer to  input string
+ * @delim: Delimiter character used for tokenizing
+ *
+ * Return: Pointer to the created token
+ */
+
+char *_strtok(char *line, char *delim)
+{
+	int i;
+	static char *current_string;
+	char *copy_string;
+
+	if (line != NULL)
+		current_string = line;
+
+	for (; *current_string != '\0'; current_string++)
+	{
+		for (i = 0; delim[i] != '\0'; i++)
+		{
+			if (*current_string == delim[i])
+				break;
+		}
+		if (delim[i] == '\0')
+			break;
+	}
+
+	copy_string = current_string;
+	if (*copy_string == '\0')
+		return (NULL);
+
+	for (; *current_str != '\0'; current_str++)
+	{
+		for (i = 0; delim[i] != '\0'; i++)
+		{
+			if (*current_str == delim[i])
+			{
+				*current_str = '\0';
+				current_str++;
+				return (copy_string);
+			}
+		}
+	}
+
+	return (copy_string);
+}
