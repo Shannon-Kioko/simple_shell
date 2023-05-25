b46b0807be80eff34fb2f92bed28ee728812f7e3
John Ombuya <79745456+wxllxngton@users.noreply.github.com>
2023-05-25 10:13:40 +0300

    Update builtins_list.c

--- builtins_list.c
+++ builtins_list.c
@@ -1,5 +1,18 @@
 #include "shell.h"
 
+typedef struct BuiltinPair {
+    char *builtin;
+    int (*function)(data_of_program *);
+} BuiltinPair;
+
+int builtin_exit(data_of_program *data);
+int builtin_help(data_of_program *data);
+int builtin_cd(data_of_program *data);
+int builtin_alias(data_of_program *data);
+int builtin_env(data_of_program *data);
+int builtin_set_env(data_of_program *data);
+int builtin_unset_env(data_of_program *data);
+
 /**
  * builtin_executes - Search for a match and execute the associated builtin.
  * @data: Pointer to the program's data struct.
@@ -9,29 +22,29 @@
  */
 int builtin_executes(data_of_program *data)
 {
-	int iterator;
-	BuiltinPair options[] = {
-		{"exit", builtin_exit},
-		{(int *)"help", builtin_help},
-		{"cd", builtin_cd},
-		{"alias", builtin_alias},
-		{"env", builtin_env},
-		{"setenv", builtin_set_env},
-		{"unsetenv", builtin_unset_env},
-		{NULL, NULL}
-	};
+    int iterator;
+    BuiltinPair options[] = {
+        {"exit", builtin_exit},
+        {"help", builtin_help},
+        {"cd", builtin_cd},
+        {"alias", builtin_alias},
+        {"env", builtin_env},
+        {"setenv", builtin_set_env},
+        {"unsetenv", builtin_unset_env},
+        {NULL, NULL}
+    };
 
-	/* Iterate through the builtin options */
-	for (iterator = 0; options[iterator].builtin != NULL; iterator++)
-	{
-		/* Check for a match between the given command and a builtin */
-		if (str_compare(options[iterator].builtin, data->command_name, 0))
-		{
-			/* Execute the associated function and return its return value */
-			return (options[iterator].function(data));
-		}
-	}
+    /* Iterate through the builtin options */
+    for (iterator = 0; options[iterator].builtin != NULL; iterator++)
+    {
+        /* Check for a match between the given command and a builtin */
+        if (str_compare(options[iterator].builtin, data->command_name, 0))
+        {
+            /* Execute the associated function and return its return value */
+            return (options[iterator].function(data));
+        }
+    }
 
-	/* If there is no match, return -1 */
-	return (-1);
+    /* If there is no match, return -1 */
+    return (-1);
 }
